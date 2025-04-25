f6205b35fcaa1daa0fee214f6c726ae0f770cee3
isololeveling <isololeveling@proton.me>
2025-04-25 17:15:38 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['cdn.noitatnemucod.net','artworks.thetvdb.com','s4.anilist.co','https://media.kitsu.app'], // Add your image domain here
+  },
+};
+
+export default nextConfig;
